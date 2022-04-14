254b5878072902c4372c11e6c61a5b51564e5d49
hieugc <hieu.phamgc@gmail.com>
2022-04-14 23:01:34 +0700

    fix dashboard

--- Dashboard_room/dashboard.js
+++ Dashboard_room/dashboard.js
@@ -8,17 +8,15 @@ nav.forEach(element => {
     })
 });
 
-google.charts.load('current', {'packages':['corechart']});
-function Draw(i_data, i_title, i_index){
-  google.charts.setOnLoadCallback(drawChart(i_data, i_title, i_index));
-  function drawChart(i_data, i_title, i_index) {
-    var data = google.visualization.arrayToDataTable([
-        ['Thời gian', 'Nhiệt độ'],
-        ['18:00 09/12/2013',  28],
-        ['2014',  30],
-        ['2015',  29],
-        ['2016',  31]
-      ]);
+function Draw(i_data, i_title, i_index, i_type){
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(function(){drawChart(i_data, i_title, i_index, i_type);});
+  function drawChart(i_data, i_title, i_index, i_type) {
+    var data = new google.visualization.DataTable();
+
+      data.addColumn('string', "Thời gian");
+      data.addColumn('number', i_type);
+      data.addRows(i_data);
     var options = {
       title: i_title,
       hAxis: {title: 'Thời gian',  titleTextStyle: {color: '#333'}},
@@ -32,7 +30,6 @@ function Draw(i_data, i_title, i_index){
 
 
 var data_1 = [
-  ['Thời gian', 'Nhiệt độ'],
   ['18:00 09/12/2013',  28],
   ['2014',  30],
   ['2015',  29],
@@ -40,11 +37,11 @@ var data_1 = [
 ];
 var title_1 = "Nhiệt độ phòng";
 var data_2 = [
-  ['Thời gian', 'Nồng độ'],
   ['18:00 09/12/2013',  28],
   ['2014',  30],
   ['2015',  29],
   ['2016',  31]
 ];
 var title_2 = "Nồng độ gas";
-Draw(data_1, title_1, 'chart_div_2');
+Draw(data_1, title_1, 'chart_div_2', "Nhiệt độ");
+Draw(data_2, title_2, 'chart_div_3', "Nhiệt độ");
