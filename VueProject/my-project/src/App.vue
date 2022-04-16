d2006ad4ad044f7b2b69e51f86a01e35a79bec54
Luan Huynh <luanhuynh2001@gmail.com>
2022-04-17 02:15:35 +0700

    init home

--- VueProject/my-project/src/App.vue
+++ VueProject/my-project/src/App.vue
@@ -2,11 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <Home />
+  
 </template>
 
 <style>
